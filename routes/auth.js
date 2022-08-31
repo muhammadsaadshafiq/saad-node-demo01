de0afe049bd62bbd181428701615956190db441b
Saad Chaudhry <saad.nextbyte@gmail.com>
2022-08-31 17:31:53 +0500

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const {name} = req.body;
+    if(name){
+        return res.status(200).send(`Welcome ${name}`);
+
+    }
+    else{
+
+        res.status(401).send('Name is required');
+    }
+});
+
+module.exports = router;
